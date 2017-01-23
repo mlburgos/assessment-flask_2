22fee41858c64879a7aa67abaec6a883932cca91
Monica Burgos <monica.l.burgos@gmail.com>
2017-01-23 04:31:58 +0000

    Completed assessment.

--- server.py
+++ server.py
@@ -10,6 +10,35 @@ app.secret_key = "ABC"
 
 
 # YOUR ROUTES GO HERE
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+
+@app.route('/application-form')
+def application():
+    jobs = ['Software Engineer', 'QA Engineer', 'Product Manager']
+    return render_template("application-form2.html",
+                           jobs=jobs,
+                           )
+
+
+@app.route('/application-success', methods=["POST"])
+def app_success():
+    name = request.form.get('firstname') + " " + request.form.get('lastname')
+    title = request.form.get('title')
+    salary_req = float(request.form.get('salaryRequirement'))
+
+    if salary_req == round(salary_req, 0):
+        formatted_salary_req = "{:20,.0f}".format(salary_req)
+    else:
+        formatted_salary_req = "{:20,.2f}".format(salary_req)
+
+    return render_template("application-response.html",
+                                   name=name,
+                                   title=title,
+                                   salary_req=formatted_salary_req,
+                                   )
 
 
 if __name__ == "__main__":
